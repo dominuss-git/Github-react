828651c8f377c6fff9a1ad30543e7737b1a14fc4
Dominuss-afk <kiril6701@gmail.com>
2020-07-12 19:38:49 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import $ from 'jquery';
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import App from './components/App.js';
+import './css/style.css';
+
+ReactDom.render(
+  <App/>,
+  document.querySelector('#app')
+);
